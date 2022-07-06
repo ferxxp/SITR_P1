df655b1592a5c3ed193d7b9eae03666c1c72ebb9
ferxxp <ferxxp@github.com>
2022-07-06 10:17:09 +0000

    first commit

---
+++ func1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <func1.h>
+void printFunc1(void) {
+printf("Function 1\n");
+return;
+}
---
+++ func2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <func2.h>
+void printFunc2(void) {
+printf("Function 2\n");
+return;
+}
